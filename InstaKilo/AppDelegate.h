fc90ab38485ebff7de665ad4dc15c5a097d600ad
Graeme Harrison <gharrison365@gmail.com>
2016-01-27 11:26:00 -0500

    First commit

---
+++ InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by Graeme Harrison on 2016-01-27.
+//  Copyright © 2016 Graeme Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
